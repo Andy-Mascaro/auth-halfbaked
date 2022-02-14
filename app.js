33894653460070fd3d06c67d5fc63f01d3c3d7b1
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-14 12:26:33 -0800

    added signup form to appjs

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
+import { signupUser,getUser } from "./fetch-utils.js";
 
 const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
+const user = await getUser();
+// export async function signupUser(email)
+// if (user){
+//     location.replace('/other-page');
+// }
+
+signUpForm.addEventListener ('submit', async (e) =>{
+    e.preventDefault();
+    console.log(signUpEmail.value, signUpPassword.value);
+    await signupUser(signUpEmail.value, signUpPassword.value);
+    // redirectIfLoggedIn();
+})
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,3 @@
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
+
