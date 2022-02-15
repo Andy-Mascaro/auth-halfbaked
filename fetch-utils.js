86f9691019a926dded03f92238705e20a5ca91be
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-14 16:17:09 -0800

    cleaned up fetch utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,4 +1,4 @@
-// Enter Supabase Information
+
 const SUPABASE_URL = 'https://kdukidihcdlbgresawun.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkdWtpZGloY2RsYmdyZXNhd3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NDYsImV4cCI6MTk1OTkxNzQ0Nn0.YtwzCCX2zDt5IzpJ-Uh5Hy4DsanDjeq6lfG72ezSCW4';
 
@@ -6,7 +6,7 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
-    // if (!user) location.replace('../');
+   
 }
 
 export async function signupUser(email, password) {
@@ -40,7 +40,7 @@ export async function redirectIfLoggedIn() {
 
 export async function logout() {
     await client.auth.signOut();
-    
+
     return window.location.href = '../';
 
 
